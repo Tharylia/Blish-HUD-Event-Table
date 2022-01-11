7c0023fa6ea1cd943dfeb3a81ee33f3e9c78f752
Dominik Beste <dominik.beste@gmx.de>
2022-01-11 18:52:29 +0100

    Code cleanup

--- Estreya.BlishHUD.EventTable/Helpers/ColorHelper.cs
+++ Estreya.BlishHUD.EventTable/Helpers/ColorHelper.cs
@@ -2,10 +2,6 @@
 {
     using Microsoft.Xna.Framework;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     internal static class ColorHelper
     {
@@ -14,7 +10,9 @@
             HSLColor hsl = HSLColor.FromColor(color);
             hsl.H += 0.5f;
             if (hsl.H > 1)
+            {
                 hsl.H -= 1f;
+            }
 
             Color switchedColor = hsl.ToRgbColor();
             switchedColor.A = color.A;
@@ -22,45 +20,62 @@
 
             // To HSL
 
-            var r = color.R / 255f;
-            var g = color.G / 255f;
-            var b = color.B / 255f;
-            var min = Math.Min(Math.Min(r, g), b);
-            var max = Math.Max(Math.Max(r, g), b);
+            float r = color.R / 255f;
+            float g = color.G / 255f;
+            float b = color.B / 255f;
+            float min = Math.Min(Math.Min(r, g), b);
+            float max = Math.Max(Math.Max(r, g), b);
 
             float h = 0f;
-            var s = 0f;
-            var l = (max + min) / 2;
+            float s = 0f;
+            float l = (max + min) / 2;
 
             if (max != 0)
             {
                 if (l < 0.5)
+                {
                     s = max / (max + min);
+                }
                 else
+                {
                     s = max / (2 - max - min);
+                }
 
-                var delta_r = (((max - r) / 6) + (max / 2)) / max;
-                var delta_g = (((max - g) / 6) + (max / 2)) / max;
-                var delta_b = (((max - b) / 6) + (max / 2)) / max;
+                float delta_r = (((max - r) / 6) + (max / 2)) / max;
+                float delta_g = (((max - g) / 6) + (max / 2)) / max;
+                float delta_b = (((max - b) / 6) + (max / 2)) / max;
 
                 if (r == max)
+                {
                     h = delta_b - delta_g;
+                }
                 else if (g == max)
+                {
                     h = (1 / 3) + delta_r - delta_b;
+                }
                 else if (b == max)
+                {
                     h = (2 / 3) + delta_g - delta_r;
+                }
 
                 if (h < 0)
+                {
                     h += 1;
+                }
+
                 if (h > 1)
+                {
                     h -= 1;
+                }
             }
 
             // Hue 180°
 
             h += 0.5f;
             if (h > 1)
+            {
                 h -= 1;
+            }
 
             // Back to Color
 
@@ -69,32 +84,49 @@
                 return new Color(l * 255, l * 255, l * 255);
             }
 
-            var two = 0.0f;
+            float two = 0.0f;
 
             if (l < 0.5)
+            {
                 two = l * (1 + s);
+            }
             else
+            {
                 two = (l + s) - (s * l);
+            }
 
-            var one = 2 * l - two;
+            float one = 2 * l - two;
 
-            return new Color(255 * HueToRGB(one, two, h + (1/3)), 255* HueToRGB(one, two, h), 255 * HueToRGB(one,two, h - (1/3)));
+            return new Color(255 * HueToRGB(one, two, h + (1 / 3)), 255 * HueToRGB(one, two, h), 255 * HueToRGB(one, two, h - (1 / 3)));
 
         }
 
         private static float HueToRGB(float one, float two, float hue)
         {
             if (hue < 0)
+            {
                 hue += 1;
+            }
+
             if (hue > 1)
+            {
                 hue -= 1;
+            }
 
             if ((6 * hue) < 1)
+            {
                 return one + (two - one) * 6 * hue;
+            }
+
             if ((2 * hue) < 1)
+            {
                 return two;
+            }
+
             if ((3 * hue) < 2)
+            {
                 return one + (two - one) * ((2 / 3) - hue) * 6;
+            }
 
             return one;
         }
@@ -125,9 +157,9 @@
 
             public HSLColor(float h, float s, float l)
             {
-                H = h;
-                S = s;
-                L = l;
+                this.H = h;
+                this.S = s;
+                this.L = l;
             }
 
             public static HSLColor FromColor(Color color)
@@ -138,11 +170,12 @@
 
             public static HSLColor FromRgb(byte R, byte G, byte B)
             {
-                var hsl = new HSLColor();
-
-                hsl.H = 0;
-                hsl.S = 0;
-                hsl.L = 0;
+                HSLColor hsl = new HSLColor
+                {
+                    H = 0,
+                    S = 0,
+                    L = 0
+                };
 
                 float r = R / 255f;
                 float g = G / 255f;
@@ -203,37 +236,37 @@
                 // complementary colors are across the color wheel
                 // which is 180 degrees or 50% of the way around the
                 // wheel. Add 50% to our hue and wrap large/small values
-                var h = H + 0.5f;
+                float h = this.H + 0.5f;
                 if (h > 1)
                 {
                     h -= 1;
                 }
 
-                return new HSLColor(h, S, L);
+                return new HSLColor(h, this.S, this.L);
             }
 
             public Color ToRgbColor()
             {
-                var c = new Color();
+                Color c = new Color();
 
-                if (S == 0)
+                if (this.S == 0)
                 {
-                    c.R = (byte)(L * 255f);
-                    c.G = (byte)(L * 255f);
-                    c.B = (byte)(L * 255f);
+                    c.R = (byte)(this.L * 255f);
+                    c.G = (byte)(this.L * 255f);
+                    c.B = (byte)(this.L * 255f);
                 }
                 else
                 {
-                    float v2 = (L + S) - (S * L);
-                    if (L < 0.5f)
+                    float v2 = (this.L + this.S) - (this.S * this.L);
+                    if (this.L < 0.5f)
                     {
-                        v2 = L * (1 + S);
+                        v2 = this.L * (1 + this.S);
                     }
-                    float v1 = 2f * L - v2;
+                    float v1 = 2f * this.L - v2;
 
-                    c.R = (byte)(255f * HueToRgb(v1, v2, H + (1f / 3f)));
-                    c.G = (byte)(255f * HueToRgb(v1, v2, H));
-                    c.B = (byte)(255f * HueToRgb(v1, v2, H - (1f / 3f)));
+                    c.R = (byte)(255f * HueToRgb(v1, v2, this.H + (1f / 3f)));
+                    c.G = (byte)(255f * HueToRgb(v1, v2, this.H));
+                    c.B = (byte)(255f * HueToRgb(v1, v2, this.H - (1f / 3f)));
                 }
 
                 return c;
@@ -247,17 +280,17 @@
 
                 if ((6 * vH) < 1)
                 {
-                    ret = (v1 + (v2 - v1) * 6 * vH);
+                    ret = v1 + (v2 - v1) * 6 * vH;
                 }
 
                 else if ((2 * vH) < 1)
                 {
-                    ret = (v2);
+                    ret = v2;
                 }
 
                 else if ((3 * vH) < 2)
                 {
-                    ret = (v1 + (v2 - v1) * ((2f / 3f) - vH) * 6f);
+                    ret = v1 + (v2 - v1) * ((2f / 3f) - vH) * 6f;
                 }
 
                 return Math.Max(Math.Min(ret, 1), 0);
