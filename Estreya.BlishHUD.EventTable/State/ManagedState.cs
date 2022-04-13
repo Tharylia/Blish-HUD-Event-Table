60fb8c88caf6721308fb8376572dc9698005ea7e
Dominik Beste <dominik.beste@gmx.de>
2022-04-13 13:40:32 +0200

    Fix async saving

--- Estreya.BlishHUD.EventTable/State/ManagedState.cs
+++ Estreya.BlishHUD.EventTable/State/ManagedState.cs
@@ -1,11 +1,16 @@
 ﻿namespace Estreya.BlishHUD.EventTable.State
 {
+    using Blish_HUD;
     using Microsoft.Xna.Framework;
     using System;
+    using System.Threading;
     using System.Threading.Tasks;
 
     public abstract class ManagedState : IDisposable
     {
+        private static readonly Logger Logger = Logger.GetLogger<ManagedState>();
+
+        private SemaphoreSlim _saveSemaphore = new SemaphoreSlim(1, 1);
         private int SaveInternal { get; set; }
 
         private TimeSpan TimeSinceSave { get; set; } = TimeSpan.Zero;
@@ -51,11 +56,27 @@
 
             if (this.TimeSinceSave.TotalMilliseconds >= this.SaveInternal)
             {
-                Task.Run(async () =>
+                // Prevent multiple threads running Save() at the same time.
+                if (_saveSemaphore.CurrentCount > 0)
                 {
-                    await this.Save();
-                    this.TimeSinceSave = TimeSpan.Zero;
-                });
+                    _ = Task.Run(async () =>
+                    {
+                        try
+                        {
+                            await _saveSemaphore.WaitAsync();
+                            await this.Save();
+                            this.TimeSinceSave = TimeSpan.Zero;
+                        }
+                        finally
+                        {
+                            _saveSemaphore.Release();
+                        }
+                    });
+                }
+                else
+                {
+                    Logger.Debug("Another thread is already running Save()");
+                }
             }
 
             this.InternalUpdate(gameTime);
