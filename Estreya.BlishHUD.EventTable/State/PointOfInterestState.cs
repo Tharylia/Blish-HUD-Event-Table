089038c72e3854b8aa82a4f8f27c5c4310cfa9ac
Dominik Beste <41393569+Tharylia@users.noreply.github.com>
2022-06-22 17:18:48 +0200

    Rework to APIState

--- Estreya.BlishHUD.EventTable/State/PointOfInterestState.cs
+++ Estreya.BlishHUD.EventTable/State/PointOfInterestState.cs
@@ -12,116 +12,152 @@ using Microsoft.Xna.Framework;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
-public class PointOfInterestState : ManagedState
+public class PointOfInterestState : APIState<PointOfInterest>
 {
     private static readonly Logger Logger = Logger.GetLogger<PointOfInterestState>();
     private const string BASE_FOLDER_STRUCTURE = "pois";
     private const string LAST_UPDATED_FILE_NAME = "last_updated.txt";
 
-    private readonly Gw2ApiManager _apiManager;
+    private const string DATE_TIME_FORMAT = "yyyy-MM-ddTHH:mm:ss";
+
     private readonly string _baseFolderPath;
-    private AsyncLock _pointOfInterestsLock = new AsyncLock();
 
     private string FullPath => Path.Combine(this._baseFolderPath, BASE_FOLDER_STRUCTURE);
 
     public bool Loading { get; private set; }
 
-    public List<PointOfInterest> PointOfInterests { get; } = new List<PointOfInterest>();
+    // Don't await loading as this can take a long time.
 
-    public PointOfInterestState(Gw2ApiManager apiManager, string baseFolderPath) : base(true, -1) // Don't save in interval, must be manually triggered
+    public PointOfInterestState(Gw2ApiManager apiManager, string baseFolderPath) : base(apiManager, updateInterval: Timeout.InfiniteTimeSpan, awaitLoad: false) // Don't save in interval, must be manually triggered
     {
-        this._apiManager = apiManager;
         this._baseFolderPath = baseFolderPath;
-    }
 
-    public override Task Clear()
-    {
-        using (this._pointOfInterestsLock.Lock())
+        this.FetchAction = async (apiManager) =>
         {
-            this.PointOfInterests.Clear();
-        }
+            List<PointOfInterest> pointOfInterests = new List<PointOfInterest>();
 
-        return Task.CompletedTask;
-    }
+            // Continent 1 = Tyria
+            // Continent 2 = Mists
 
-    public override async Task InternalReload()
-    {
-        await this.Clear();
-        await this.Load();
-    }
+            Gw2Sharp.WebApi.V2.IApiV2ObjectList<Continent> continents = await apiManager.Gw2ApiClient.V2.Continents.AllAsync();
 
-    protected override Task Initialize()
-    {
-        return Task.CompletedTask;
-    }
+            foreach (ContinentDetails continent in continents.Select(x => new ContinentDetails(x)))
+            {
+                Gw2Sharp.WebApi.V2.IApiV2ObjectList<ContinentFloor> floors = await apiManager.Gw2ApiClient.V2.Continents[continent.Id].Floors.AllAsync();
 
-    protected override void InternalUnload()
-    {
-        AsyncHelper.RunSync(this.Clear);
-    }
+                foreach (ContinentFloor floor in floors)
+                {
+                    ContinentFloorDetails floorDetails = new ContinentFloorDetails(floor);
 
-    protected override void InternalUpdate(GameTime gameTime)
-    {
+                    foreach (ContinentFloorRegion region in floor.Regions.Values)
+                    {
+                        ContinentFloorRegionDetails regionDetails = new ContinentFloorRegionDetails(region);
+
+                        foreach (ContinentFloorRegionMap map in region.Maps.Values)
+                        {
+                            ContinentFloorRegionMapDetails mapDetails = new ContinentFloorRegionMapDetails(map);
+
+                            foreach (ContinentFloorRegionMapPoi pointOfInterest in map.PointsOfInterest.Values.Where(poi => poi.Name != null))
+                            {
+                                PointOfInterest landmark = new PointOfInterest(pointOfInterest)
+                                {
+                                    Continent = continent,
+                                    Floor = floorDetails,
+                                    Region = regionDetails,
+                                    Map = mapDetails
+                                };
+
+                                pointOfInterests.Add(landmark);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return pointOfInterests.DistinctBy(poi => new { poi.Name }).ToList();
+        };
     }
 
     protected override async Task Load()
     {
-        bool loadFromApi = false;
-
-        if (Directory.Exists(this.FullPath))
+        lock (this)
         {
-            bool continueLoadingFiles = true;
+            this.Loading = true;
+        }
 
-            string lastUpdatedFilePath = Path.Combine(this.FullPath, LAST_UPDATED_FILE_NAME);
-            if (!System.IO.File.Exists(lastUpdatedFilePath))
-            {
-                await this.CreateLastUpdatedFile();
-            }
+        try
+        {
+            bool loadFromApi = false;
 
-            string dateString = await FileUtil.ReadStringAsync(lastUpdatedFilePath);
-            if (!DateTime.TryParse(dateString, out DateTime lastUpdated))
+            if (Directory.Exists(this.FullPath))
             {
-                Logger.Debug("Failed parsing last updated.");
-            }
-            else
-            {
-                if (DateTime.UtcNow - new DateTime(lastUpdated.Ticks, DateTimeKind.Utc) > TimeSpan.FromDays(5))
+                bool continueLoadingFiles = true;
+
+                string lastUpdatedFilePath = Path.Combine(this.FullPath, LAST_UPDATED_FILE_NAME);
+                if (!System.IO.File.Exists(lastUpdatedFilePath))
                 {
-                    continueLoadingFiles = false;
-                    loadFromApi = true;
+                    await this.CreateLastUpdatedFile();
                 }
-            }
 
-            if (continueLoadingFiles)
-            {
-                string[] files = Directory.GetFiles(this.FullPath, "*.*", SearchOption.AllDirectories);
-
-                if (files.Length > 0)
+                string dateString = await FileUtil.ReadStringAsync(lastUpdatedFilePath);
+                if (!DateTime.TryParseExact(dateString, DATE_TIME_FORMAT, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime lastUpdated))
                 {
-                    await this.LoadFromFiles(files);
+                    Logger.Debug("Failed parsing last updated.");
                 }
                 else
                 {
-                    loadFromApi = true;
+                    if (DateTime.UtcNow - new DateTime(lastUpdated.Ticks, DateTimeKind.Utc) > TimeSpan.FromDays(5))
+                    {
+                        continueLoadingFiles = false;
+                        loadFromApi = true;
+                    }
+                }
+
+                if (continueLoadingFiles)
+                {
+                    var dirs = Directory.GetDirectories(this.FullPath).ToList();
+
+                    string[] files = dirs.SelectMany(dir => Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories)).ToArray();
+
+                    if (files.Length > 0)
+                    {
+                        await this.LoadFromFiles(files);
+                    }
+                    else
+                    {
+                        loadFromApi = true;
+                    }
                 }
             }
-        }
-        else
-        {
-            loadFromApi = true;
-        }
+            else
+            {
+                loadFromApi = true;
+            }
 
-        if (loadFromApi)
-        {
-            await this.LoadFromAPI();
-            await this.Save();
-        }
+            if (loadFromApi)
+            {
+                await base.Load();
+                await this.Save();
+            }
 
-        Logger.Debug("Loaded {0} point of interests.", this.PointOfInterests.Count);
+            Logger.Debug("Loaded {0} point of interests.", this.APIObjectList.Count);
+        }catch (Exception ex)
+        {
+            Logger.Warn(ex, "Failed loading point of interests:");
+        }
+        finally
+        {
+            lock (this)
+            {
+                this.Loading = false;
+            }
+        }
     }
 
     private async Task LoadFromFiles(string[] files)
@@ -139,7 +175,7 @@ public class PointOfInterestState : ManagedState
 
         _ = await Task.WhenAll(loadTasks);
 
-        using (await this._pointOfInterestsLock.LockAsync())
+        using (await this._listLock.LockAsync())
         {
             foreach (Task<string> loadTask in loadTasks)
             {
@@ -150,9 +186,16 @@ public class PointOfInterestState : ManagedState
                     continue;
                 }
 
-                PointOfInterest poi = JsonConvert.DeserializeObject<PointOfInterest>(result);
+                try
+                {
+                    PointOfInterest poi = JsonConvert.DeserializeObject<PointOfInterest>(result);
 
-                this.PointOfInterests.Add(poi);
+                    this.APIObjectList.Add(poi);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Warn(ex, "Could not parse poi: {0}", result.Replace("\n", "").Replace("\r", ""));
+                }
             }
         }
     }
@@ -166,13 +209,13 @@ public class PointOfInterestState : ManagedState
 
         _ = Directory.CreateDirectory(this.FullPath);
 
-        using (await this._pointOfInterestsLock.LockAsync())
+        using (await this._listLock.LockAsync())
         {
-            IEnumerable<Task> fileWriteTasks = this.PointOfInterests.Select(poi =>
+            IEnumerable<Task> fileWriteTasks = this.APIObjectList.Select(poi =>
             {
                 string landmarkPath = Path.Combine(this.FullPath, FileUtil.SanitizeFileName(poi.Continent.Name), FileUtil.SanitizeFileName(poi.Floor.Id.ToString()), FileUtil.SanitizeFileName(poi.Region.Name), FileUtil.SanitizeFileName(poi.Map.Name), FileUtil.SanitizeFileName(poi.Name) + ".txt");
 
-                Directory.CreateDirectory(Path.GetDirectoryName(landmarkPath));
+                _ = Directory.CreateDirectory(Path.GetDirectoryName(landmarkPath));
 
                 string landmarkData = JsonConvert.SerializeObject(poi, Formatting.Indented);
 
@@ -187,79 +230,20 @@ public class PointOfInterestState : ManagedState
 
     private async Task CreateLastUpdatedFile()
     {
-        await FileUtil.WriteStringAsync(Path.Combine(this.FullPath, LAST_UPDATED_FILE_NAME), DateTime.UtcNow.ToString());
+        await FileUtil.WriteStringAsync(Path.Combine(this.FullPath, LAST_UPDATED_FILE_NAME), DateTime.UtcNow.ToString(DATE_TIME_FORMAT));
     }
 
     public PointOfInterest GetPointOfInterest(string chatCode)
     {
-        using (this._pointOfInterestsLock.Lock())
+        using (this._listLock.Lock())
         {
-            IEnumerable<PointOfInterest> foundPointOfInterests = this.PointOfInterests.Where(wp => wp.ChatLink == chatCode);
+            IEnumerable<PointOfInterest> foundPointOfInterests = this.APIObjectList.Where(wp => wp.ChatLink == chatCode);
 
             return foundPointOfInterests.Any() ? foundPointOfInterests.First() : null;
         }
     }
 
-    private async Task LoadFromAPI()
-    {
-        try
-        {
-            this.Loading = true;
+    public override Task DoClear() => Task.CompletedTask;
 
-            await this.Clear();
-
-            using (await this._pointOfInterestsLock.LockAsync())
-            {
-                List<PointOfInterest> pointOfInterests = new List<PointOfInterest>();
-
-                // Continent 1 = Tyria
-                // Continent 2 = Mists
-
-                Gw2Sharp.WebApi.V2.IApiV2ObjectList<Continent> continents = await this._apiManager.Gw2ApiClient.V2.Continents.AllAsync();
-
-                foreach (ContinentDetails continent in continents.Select(x => new ContinentDetails(x)))
-                {
-                    Gw2Sharp.WebApi.V2.IApiV2ObjectList<ContinentFloor> floors = await this._apiManager.Gw2ApiClient.V2.Continents[continent.Id].Floors.AllAsync();
-
-                    foreach (ContinentFloor floor in floors)
-                    {
-                        ContinentFloorDetails floorDetails = new ContinentFloorDetails(floor);
-
-                        foreach (ContinentFloorRegion region in floor.Regions.Values)
-                        {
-                            ContinentFloorRegionDetails regionDetails = new ContinentFloorRegionDetails(region);
-
-                            foreach (ContinentFloorRegionMap map in region.Maps.Values)
-                            {
-                                ContinentFloorRegionMapDetails mapDetails = new ContinentFloorRegionMapDetails(map);
-
-                                foreach (ContinentFloorRegionMapPoi pointOfInterest in map.PointsOfInterest.Values.Where(poi => poi.Name != null))
-                                {
-                                    PointOfInterest landmark = new PointOfInterest(pointOfInterest)
-                                    {
-                                        Continent = continent,
-                                        Floor = floorDetails,
-                                        Region = regionDetails,
-                                        Map = mapDetails
-                                    };
-
-                                    pointOfInterests.Add(landmark);
-                                }
-                            }
-                        }
-                    }
-                }
-
-                this.PointOfInterests.AddRange(pointOfInterests.DistinctBy(poi => new { poi.Name })); // POIS can exists on multiple levels with the same data
-            }
-        }
-        catch (Exception ex)
-        {
-            Logger.Warn(ex, "Could not load point of interests:");
-        }
-        finally
-        {
-            this.Loading = false;
-        }
-    }
+    protected override void DoUnload() { /* NOOP */ }
 }
